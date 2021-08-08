b9fcab733216f3d5af1b6255c2a44f279dec7f12
Marek Sroczkowski <marekes97@gmail.com>
2021-08-08 17:54:04 +0200

    Improved dependency injection installation mechanism

---
+++ TutoringSystem/TutoringSystemAPI/Installers/MvcInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TutoringSystem.Application.DependencyInjection;
+using TutoringSystem.Infrastructure.DependencyInjection;
+
+namespace TutoringSystem.API.Installers
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddApplication();
+            services.AddInfrastructure();
+
+            services.AddControllers();
+        }
+    }
+}
