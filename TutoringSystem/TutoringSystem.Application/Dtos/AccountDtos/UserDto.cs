d8d12f7c72611640a0793dc2a1799dcdf8b5015b
Marek Sroczkowski <marekes97@gmail.com>
2021-08-09 22:37:13 +0200

    Added dtos related to account along with the mapping profile

---
+++ TutoringSystem/TutoringSystem.Application/Dtos/AccountDtos/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using TutoringSystem.Application.Mapping;
+using TutoringSystem.Domain.Entities;
+using TutoringSystem.Domain.Entities.Enums;
+
+namespace TutoringSystem.Application.Dtos.AccountDtos
+{
+    public class UserDto : IMap
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public Role Role { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<User, UserDto>();
+        }
+    }
+}
---
+++ TutoringSystem/TutoringSystem.Application/Mapping/IMap.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace TutoringSystem.Application.Mapping
+{
+    public interface IMap
+    {
+        void Mapping(Profile profile);
+    }
+}
