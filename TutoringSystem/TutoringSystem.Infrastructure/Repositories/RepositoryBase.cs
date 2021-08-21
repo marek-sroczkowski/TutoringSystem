43757027be7bb1b45e6d72209dc27b631876d1d6
Marek Sroczkowski <marekes97@gmail.com>
2021-08-21 21:56:16 +0200

    Added abstraction for repositories

---
+++ TutoringSystem/TutoringSystem.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using TutoringSystem.Domain.Repositories;
+using TutoringSystem.Infrastructure.Data;
+
+namespace TutoringSystem.Infrastructure.Repositories
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+	{
+		protected AppDbContext DbContext { get; set; }
+
+        protected RepositoryBase(AppDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public IQueryable<T> FindAll()
+		{
+			return DbContext.Set<T>()
+				.AsNoTracking();
+		}
+
+		public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+		{
+			return DbContext.Set<T>()
+				.Where(expression)
+				.AsNoTracking();
+		}
+
+		public void Create(T entity)
+		{
+			DbContext.Set<T>().Add(entity);
+		}
+
+		public void Update(T entity)
+		{
+			DbContext.Set<T>().Update(entity);
+		}
+
+		public void Delete(T entity)
+		{
+			DbContext.Set<T>().Remove(entity);
+		}
+	}
+}
