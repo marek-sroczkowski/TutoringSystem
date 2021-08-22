5102f0875c36358fc5b05b7bf8f89f8dddc00b00
Marek Sroczkowski <marekes97@gmail.com>
2021-08-22 23:38:40 +0200

    Created validate order existene

---
+++ TutoringSystem/TutoringSystem.Application/Filters/ValidateOrderExistenceAttribute.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+using TutoringSystem.Domain.Repositories;
+
+namespace TutoringSystem.Application.Filters
+{
+    public class ValidateOrderExistenceAttribute : TypeFilterAttribute
+    {
+        public ValidateOrderExistenceAttribute() : base(typeof(ValidateOrderExistenceFilterImpl))
+        {
+        }
+
+        private class ValidateOrderExistenceFilterImpl : IAsyncActionFilter
+        {
+            private readonly IAdditionalOrderRepository additionalOrderRepository;
+
+            public ValidateOrderExistenceFilterImpl(IAdditionalOrderRepository additionalOrderRepository)
+            {
+                this.additionalOrderRepository = additionalOrderRepository;
+            }
+
+            public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+            {
+                if (context.ActionArguments.ContainsKey("orderId"))
+                {
+                    var orderId = context.ActionArguments["orderId"] as int?;
+                    if (orderId.HasValue)
+                    {
+                        if ((await additionalOrderRepository.GetAdditionalOrderByIdAsync(orderId.Value)) == null)
+                        {
+                            context.Result = new NotFoundObjectResult(orderId.Value);
+                            return;
+                        }
+                    }
+                }
+                await next();
+            }
+        }
+    }
+}
