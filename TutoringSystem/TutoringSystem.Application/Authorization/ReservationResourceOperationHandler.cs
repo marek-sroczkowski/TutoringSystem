1ccab9d45ec27cd44e855c8b6c27cf23d7e07434
Marek Sroczkowski <marekes97@gmail.com>
2021-09-14 23:33:22 +0200

    Added authorization for reservation

---
+++ TutoringSystem/TutoringSystem.Application/Authorization/ReservationResourceOperationHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TutoringSystem.Application.Dtos.ReservationDtos;
+
+namespace TutoringSystem.Application.Authorization
+{
+    class ReservationResourceOperationHandler : AuthorizationHandler<ResourceOperationRequirement, ReservationDetailsDto>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ResourceOperationRequirement requirement, ReservationDetailsDto resource)
+        {
+            if (requirement.OperationType == OperationType.Create)
+            {
+                context.Succeed(requirement);
+            }
+
+            var userId = context.User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value;
+
+            if (resource.Tutor.Id == long.Parse(userId) || resource.Student.Id == long.Parse(userId))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
