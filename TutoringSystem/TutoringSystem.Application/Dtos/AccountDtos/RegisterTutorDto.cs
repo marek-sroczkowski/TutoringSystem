075a670badf8a0da70cfb323ae1fda4c7b2c48f2
Marek Sroczkowski <marekes97@gmail.com>
2021-10-30 22:59:51 +0200

    Removed last name from register tutor dto

--- TutoringSystem/TutoringSystem.Application/Dtos/AccountDtos/RegisterTutorDto.cs
+++ TutoringSystem/TutoringSystem.Application/Dtos/AccountDtos/RegisterTutorDto.cs
@@ -8,7 +8,6 @@ namespace TutoringSystem.Application.Dtos.AccountDtos
     {
         public string Username { get; set; }
         public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
