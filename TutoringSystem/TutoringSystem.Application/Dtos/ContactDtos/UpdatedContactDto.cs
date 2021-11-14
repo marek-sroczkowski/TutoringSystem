0edac9e993f3a354d57fd76c59f01977d19428c9
Marek Sroczkowski <marekes97@gmail.com>
2021-11-14 18:44:50 +0100

    Changed updated contact dto

--- TutoringSystem/TutoringSystem.Application/Dtos/ContactDtos/UpdatedContactDto.cs
+++ TutoringSystem/TutoringSystem.Application/Dtos/ContactDtos/UpdatedContactDto.cs
@@ -1,6 +1,4 @@
 ﻿using AutoMapper;
-using System.Collections.Generic;
-using TutoringSystem.Application.Dtos.PhoneNumberDtos;
 using TutoringSystem.Application.Mapping;
 using TutoringSystem.Domain.Entities;
 
@@ -9,10 +7,7 @@ namespace TutoringSystem.Application.Dtos.ContactDtos
     public class UpdatedContactDto : IMap
     {
         public long Id { get; set; }
-        public string Email { get; set; }
         public string DiscordName { get; set; }
-
-        public ICollection<UpdatedPhoneNumberDto> PhoneNumbers { get; set; }
 
         public void Mapping(Profile profile)
         {
