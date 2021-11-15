84aebde350dfe78161ef993af7837266517a2926
Marek Sroczkowski <marekes97@gmail.com>
2021-11-15 20:02:52 +0100

    Extended user entity with base64 profile picture

---
+++ TutoringSystem/TutoringSystem.Infrastructure/Data/20211115190117_ProfileImage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TutoringSystem.Infrastructure.Data.Migrations
+{
+    public partial class ProfileImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePictureBase64",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePictureBase64",
+                table: "Users");
+        }
+    }
+}
