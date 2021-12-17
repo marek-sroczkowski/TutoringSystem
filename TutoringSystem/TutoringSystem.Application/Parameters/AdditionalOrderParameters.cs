bd20d51c521c3d936dc73ab60a01a9eac1db01cd
Marek Sroczkowski <marekes97@gmail.com>
2021-12-18 00:19:22 +0100

    Fix in order parameters

--- TutoringSystem/TutoringSystem.Application/Parameters/AdditionalOrderParameters.cs
+++ TutoringSystem/TutoringSystem.Application/Parameters/AdditionalOrderParameters.cs
@@ -1,17 +1,35 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TutoringSystem.Application.Parameters
 {
     public class AdditionalOrderParameters : QueryStringParameters
     {
+        [Required]
         public bool IsPaid { get; set; }
+
+        [Required]
         public bool IsNotPaid { get; set; }
+
+        [Required]
         public bool IsInProgress { get; set; }
+
+        [Required]
         public bool IsPending { get; set; }
+
+        [Required]
         public bool IsRealized { get; set; }
+
+        [Required]
         public DateTime ReceiptStartDate { get; set; }
+
+        [Required]
         public DateTime ReceiptEndDate { get; set; }
+
+        [Required]
         public DateTime DeadlineStart { get; set; }
+
+        [Required]
         public DateTime DeadlineEnd { get; set; }
     }
 }
