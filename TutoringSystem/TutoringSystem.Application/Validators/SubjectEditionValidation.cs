784a53b5600ca75b9247db9da1cbe34aafcc4637
Marek Sroczkowski <marek.sroczkowski.dev@gmail.com>
2022-02-27 21:32:19 +0100

    Added fluent validators for subject creation and edition

---
+++ TutoringSystem/TutoringSystem.Application/Validators/SubjectEditionValidation.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using TutoringSystem.Application.Dtos.SubjectDtos;
+using TutoringSystem.Application.Extensions;
+using TutoringSystem.Domain.Repositories;
+
+namespace TutoringSystem.Application.Validators
+{
+    public class SubjectEditionValidation : AbstractValidator<UpdatedSubjectDto>
+    {
+        public SubjectEditionValidation(ISubjectRepository subjectRepository, IHttpContextAccessor httpContext)
+        {
+            RuleFor(s => s.Name).Custom((value, context) =>
+            {
+                var userId = httpContext.HttpContext.User.GetUserId();
+                if (subjectRepository.IsSubjectExist(s => s.TutorId.Equals(userId) && s.Name.Equals(value)))
+                {
+                    context.AddFailure("name", "That subject name is taken");
+                }
+            });
+        }
+    }
+}
